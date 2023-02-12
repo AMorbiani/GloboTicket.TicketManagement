405cb0706419101f1d4ca3cd0d1bbe39d401b696
AMorbiani0 <alberto.morbiani@gmail.com>
2023-02-12 20:59:03 +0100

    Adding Persistance and Infrastructure

--- GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommand.cs
+++ GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommand.cs
@@ -1,12 +1,14 @@
 ﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Infrastructure;
 using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Application.Models.Mail;
 using GloboTicket.TicketManagement.Domain.Entities;
 using MediatR;
 using System;
 
 namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.CreateEvent
 {
-    public class CreateEventCommand: IRequest<Guid>
+    public class CreateEventCommand : IRequest<Guid>
     {
         public string Name { get; set; } = string.Empty;
         public int Price { get; set; }
@@ -25,12 +27,14 @@ namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.Crea
     {
         private readonly IEventRepository _eventRepository;
         private readonly IMapper _mapper;
+        private readonly IEmailService _emailService;
 
 
-        public CreateEventCommandHandler(IMapper mapper, IEventRepository eventRepository)
+        public CreateEventCommandHandler(IMapper mapper, IEventRepository eventRepository, IEmailService emailService)
         {
             _mapper = mapper;
             _eventRepository = eventRepository;
+            _emailService = emailService;
         }
 
         public async Task<Guid> Handle(CreateEventCommand request, CancellationToken cancellationToken)
@@ -46,6 +50,18 @@ namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.Crea
 
             @event = await _eventRepository.AddAsync(@event);
 
+            // Send Email
+            var email = new Email() { To = "bertorhoita@yahoo.it", Body = $"A new event was created: {request}", Subject = "A new event was created" };
+
+            try
+            {
+                await _emailService.SendEmai(email);
+            }
+            catch
+            {
+                // Don't stop the API
+                // Possible Log
+            }
             return @event.EventId;
         }
     }
---
+++ GloboTicket.TicketManagement.Application/Models/Mail/Email.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Application.Models.Mail
+{
+    public class Email
+    {
+        public string To { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public string Body { get; set; } = string.Empty;
+    }
+}

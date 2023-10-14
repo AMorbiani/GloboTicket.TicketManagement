97c67e7773b88c091a57af1413451139aa90d6f9
AMorbiani0 <alberto.morbiani@gmail.com>
2023-10-14 20:34:22 +0200

    Adding Blazor UI

---
+++ GloboTicket.TicketManagement.App/Contracts/IAuthenticationService.cs
@@ -0,0 +1,9 @@
+﻿namespace GloboTicket.TicketManagement.App.Contracts
+{
+    public interface IAuthenticationService
+    {
+        Task<bool> Authenticate(string email, string password);
+        Task<bool> Register(string firstName, string lastName, string userName, string email, string password);
+        Task Logout();
+    }
+}
---
+++ GloboTicket.TicketManagement.App/Program.cs
@@ -0,0 +1,36 @@
+using Blazored.LocalStorage;
+using GloboTicket.TicketManagement.App;
+using GloboTicket.TicketManagement.App.Auth;
+using GloboTicket.TicketManagement.App.Contracts;
+using GloboTicket.TicketManagement.App.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddBlazoredLocalStorage();
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+
+
+builder.Services.AddSingleton(new HttpClient
+{
+    BaseAddress = new Uri("https://localhost:7020")
+});
+
+builder.Services.AddTransient<CustomAuthorizationMessageHandler>();
+builder.Services.AddHttpClient<IClient, Client>(client => client.BaseAddress = new Uri("https://localhost:7020"))
+    .AddHttpMessageHandler<CustomAuthorizationMessageHandler>();
+
+builder.Services.AddScoped<IEventDataService, EventDataService>();
+builder.Services.AddScoped<ICategoryDataService, CategoryDataService>();
+builder.Services.AddScoped<IOrderDataService, OrderDataService>();
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+await builder.Build().RunAsync();
